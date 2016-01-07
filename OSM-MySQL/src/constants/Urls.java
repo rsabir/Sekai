9b37f110ad4c940553fd92ebc57f836b68df2396
sabir <rsabir@gmail.com>
2016-01-07 19:59:00 +0100

    small error

--- OSM-MySQL/src/constants/Urls.java
+++ OSM-MySQL/src/constants/Urls.java
@@ -1,6 +1,6 @@
 package constants;
 
 public class Urls {
-	public static String IP="172.20.0.43";
+	public static String IP="172.20.0.45";
 	public static final String CONFIGSERVER = "http://localhost:8080/Config";
 }
