75e1f5abccc760dca06e5f49ec3ad64958eb6bda
AmelSpark <araboudi@enseirb.fr>
2016-01-09 21:21:17 +0100

    Revert "Revert "adding project with init""
    This reverts commit c5b25ccc050d9f01effc3643d8fcdba505e17702.

---
+++ OSM-MySQL-A/src/servlet/Config.java
@@ -0,0 +1,85 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ * Servlet implementation class Config
+ */
+@WebServlet("/Config")
+public class Config extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Config() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String jsonString =  "{\"servers\":["+
+	             "{"+
+	                "\"zone\":{"+
+	                       "\"maxlat\": 44.8448769,"+
+	                       "\"minlat\": 44.8305549,"+
+	                      " \"maxlon\": -0.5275520000000142,"+
+	                       "\"minlon\": -0.6563579999999547"+
+	                "},"+
+	                "\"host\":\"1.2.3.4\""+
+	             "},{"+
+	                "\"zone\":{"+
+	                       "\"maxlat\": 100,"+
+	                       "\"minlat\": 30,"+
+	                       "\"maxlon\": 20,"+
+	                       "\"minlon\": 0"+
+	                "},"+
+	                "\"host\":\"1.2.3.5\""+
+	                "},{"+
+	                "\"zone\":{"+
+	                       "\"maxlat\": 30,"+
+	                       "\"minlat\": 25,"+
+	                       "\"maxlon\": 25,"+
+	                       "\"minlon\": 20"+
+	                "},"+
+	                "\"host\":\"1.2.3.6\""+
+	             "}]}";
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonResponse;
+		try {
+			jsonResponse = (JSONObject) jsonParser.parse(jsonString);
+			request.setCharacterEncoding("utf8");
+			response.setContentType("application/json");
+			PrintWriter out = response.getWriter();
+			out.print(jsonResponse);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
